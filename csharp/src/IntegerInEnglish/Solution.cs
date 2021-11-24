458cfbf3fc8752f2ffda9c05a4b0862e5d7838eb
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2021-11-24 23:10:20 +0100

    Integer to English Words

--- csharp/src/IntegerInEnglish/Solution.cs
+++ csharp/src/IntegerInEnglish/Solution.cs
@@ -1,82 +1,127 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace CodeChallenges.IntegerInEnglish
 {
     public class Solution
     {
-        
         public string NumberToWords(int num)
         {
-            var digits = EnumerateDigitsOf(num).ToArray();
-            return digits.Length switch
-            {
-                1 => SayUnit(digits[0]),
-                2 => SayTens(digits[0], digits[1]),
-                _ => string.Empty
-            };
+            return string.Join(" ", EnumerateDigitsOf(num)
+                .Chunk(3)
+                .Select((chunk, idx) => new Packet(chunk, idx * 3))
+                .Reverse()
+                .Select(p=>p.ToString())
+                .Where(s=>!string.IsNullOrEmpty(s))
+                ).Trim();
         }
-
-        private string SayTens(int tensDigit, int unitDigit)
-        {
-            var n = tensDigit * 10 + unitDigit;
-            return (tensDigit, unitDigit) switch
-            {
-                (1,_) => teens[n],
-                (_,0) => tens[n],
-                (_,_) => $"{tens[tensDigit*10]} {units[unitDigit]}",
-            };
-        }
-
-        private string SayUnit(int digit) => units[digit];
         
         private static IEnumerable<int> EnumerateDigitsOf(int n)
         {
-            IEnumerable<int> ToDigits(int i)
+            while (n >= 10)
             {
-                while (i >= 10)
-                {
-                    var d = i % 10;
-                    i = i / 10;
-                    yield return d;
-                }
+                var d = n % 10;
+                n = n / 10;
+                yield return d;
+            }
+            yield return n;
+        }
 
-                yield return i;
+ 
+
+        public record Packet(int[] digits, int startIndex)
+        {
+            public override string ToString()
+            {
+                
+                var significantDigits = digits.Length == 1 ? digits : SignificantDigits(); 
+                var pstr = significantDigits.Length switch
+                {
+                    1 => One(significantDigits),
+                    2 => Two(significantDigits),
+                    3 => Three(significantDigits),
+                    _ => string.Empty
+                };
+                return pstr != string.Empty ? $"{pstr} {powers_ten.GetValueOrDefault(startIndex, "")}".Trim():pstr;
             }
 
-            return ToDigits(n).Reverse();
+            private string One(int[] digits)
+            {
+                return units[digits[0]];
+            }
+
+            private string Two(int[] digits)
+            {
+                var (tensDigit, unitDigit) = (digits[0],digits[1]);
+                var n = tensDigit * 10 + unitDigit;
+                return (tensDigit, unitDigit) switch
+                {
+                    (0,0) => String.Empty,
+                    (1, _) => teens[n],
+                    (0, _) => units[unitDigit],
+                    (_, 0) => tens[n],
+                    (_, _) => $"{tens[tensDigit * 10]} {units[unitDigit]}",
+                };
+            }
+
+            private string Three(int[] digits)
+            {
+                return $"{units[digits[0]]} Hundred {Two(digits[1..])}".Trim();
+            }
+
+            private int[] SignificantDigits()
+            {
+                IEnumerable<int> EnumerateSignificantDigits()
+                {
+                    var isSignificant = false;
+                    for (int i = digits.Length-1; i >= 0; i--)
+                    {
+                        var digit = digits[i];
+                        if (digit != 0)
+                        {
+                            isSignificant = true;
+                        }
+
+                        if (isSignificant)
+                            yield return digit;
+
+                    }
+                }
+                return EnumerateSignificantDigits().ToArray();
+            }
+            
+            private static Dictionary<int, string> units = new()
+            {
+                [0] = "Zero",
+                [1] = "One", [2] = "Two", [3] = "Three",
+                [4] = "Four", [5] = "Five", [6] = "Six",
+                [7] = "Seven", [8] = "Eight", [9] = "Nine"
+            };
+
+            private static Dictionary<int, string> teens = new()
+            {
+                [10] = "Ten",
+                [11] = "Eleven", [12] = "Twelve", [13] = "Thirteen",
+                [14] = "Fourteen", [15] = "Fifteen", [16] = "Sixteen",
+                [17] = "Seventeen", [18] = "Eighteen", [19] = "Nineteen"
+            };
+
+            private static Dictionary<int, string> tens = new()
+            {
+            
+                [20] = "Twenty", [30] = "Thirty", [40] = "Forty",
+                [50] = "Fifty", [60] = "Sixty", [70] = "Seventy",
+                [80] = "Eighty", [90] = "Ninety"
+            };
+
+            private static Dictionary<int, string> powers_ten = new()
+            {
+                [3] = "Thousand",
+                [6] = "Million",
+                [9] = "Billion"
+            };
         }
-        
-        private static Dictionary<int, string> units = new()
-        {
-            [0] = "Zero",
-            [1] = "One", [2]= "Two", [3]= "Three", 
-            [4] = "Four", [5] = "Five", [6] = "Six", 
-            [7]= "Seven", [8]= "Eight", [9] = "Nine"
-        };
-        
-        private static Dictionary<int, string> teens = new()
-        {
-            [11] = "Eleven", [12]= "Twelve", [13]= "Thirteen", 
-            [14] = "Fourteen", [15] = "Fifteen", [16] = "Sixteen", 
-            [17]= "Seventeen", [18]= "Eighteen", [19] = "Nineteen"
-        };
-
-        private static Dictionary<int, string> tens = new()
-        {
-            [20] = "Twenty", [30] = "Thirty", [40] = "Forty",
-            [50] = "Fifty", [60] = "Sixty", [70] = "Seventy",
-            [80] = "Eighty", [90] = "Ninety"
-        };
-
-        private static Dictionary<int, string> toto = new()
-        {
-            [2] = "Hundred",
-            [4] = "Thousand",
-            [6] = "Million",
-            [9] = "Billion"
-        };
-
     }
 }
--- csharp/src/IntegerInEnglish/Tests.cs
+++ csharp/src/IntegerInEnglish/Tests.cs
@@ -7,14 +7,35 @@ namespace CodeChallenges.IntegerInEnglish
     {
         [TestCase(0, "Zero")]
         [TestCase(5, "Five")]
+        [TestCase(10, "Ten")]
         [TestCase(15, "Fifteen")]
         [TestCase(20, "Twenty")]
         [TestCase(23, "Twenty Three")]
+        [TestCase(100, "One Hundred")]
+        [TestCase(101, "One Hundred One")]
         [TestCase(123, "One Hundred Twenty Three")]
+        [TestCase(1234, "One Thousand Two Hundred Thirty Four")]
+        [TestCase(1000, "One Thousand")]
+        [TestCase(1001, "One Thousand One")]
+        [TestCase(12345, "Twelve Thousand Three Hundred Forty Five")]
+        [TestCase(10000, "Ten Thousand")]
+        [TestCase(10001, "Ten Thousand One")]
+        [TestCase(100000, "One Hundred Thousand")]
+        [TestCase(1000000, "One Million")]
+        [TestCase(1000010, "One Million Ten")]
+        [TestCase(1234567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven")]
+        [TestCase(1234567891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One")]
         public void examples(int n, string expected)
         {
             var s = new Solution();
             Check.That(s.NumberToWords(n)).IsEqualTo(expected);
+        }
+
+        [Test]
+        public void test()
+        {
+            var p = new Solution.Packet(new[] { 1, 0, 0 }, 0);
+            Check.That(p.ToString()).IsEqualTo("One");
         }
     }
 }
